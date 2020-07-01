07d12a0476bc62d8c9ebfa2f94df1f1877f0e5ee
Raqeebul Ketan <bijoy.ketan@gmail.com>
2020-07-01 16:49:54 -0400

    Added some basic test cases for mono and flux.

--- src/test/java/com/bijoyketan/springreactivepractice/PlayGround.java
+++ src/test/java/com/bijoyketan/springreactivepractice/PlayGround.java
@@ -3,13 +3,34 @@ package com.bijoyketan.springreactivepractice;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
 public class PlayGround {
 
     @Test
-    public void testWithMonoFlux() {
-        Flux.just("Amy", "Sam", "Nancy")
-                .subscribe();
+    public void monoTest() {
+        Mono<String> mono = Mono.just("Amy")
+                .log();
+
+        StepVerifier.create(mono)
+                .expectNext("Amy")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void fluxTest() {
+        Flux<String> flux = Flux.just("Canada", "USA", "China", "Mexico")
+                .log();
+
+        StepVerifier.create(flux)
+                .expectNext("Amy")
+                .expectNext("Sam")
+                .expectNext("nancy")
+                .expectNext("Lida")
+                .expectComplete()
+                .verify();
     }
 }
