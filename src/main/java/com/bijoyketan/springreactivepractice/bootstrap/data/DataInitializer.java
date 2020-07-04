5e25e500a9d6d2c514a856a2c67bb67ec47dd970
Raqeebul Ketan <bijoy.ketan@gmail.com>
2020-07-03 21:09:22 -0400

    Added a data initialization class to bootstrap the application with some starting dataset.

---
+++ src/main/java/com/bijoyketan/springreactivepractice/bootstrap/data/DataInitializer.java
@@ -0,0 +1,50 @@
+package com.bijoyketan.springreactivepractice.bootstrap.data;
+
+import com.bijoyketan.springreactivepractice.domain.Item;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class DataInitializer {
+
+    private List<String> names = Arrays.asList("Heinz", "Bose", "SanDisk", "Apple", "SamSung");
+    private List<String> colors = Arrays.asList("Red", "Blue", "Green", "Yellow", "Orange");
+    private List<List<String>> nearbyStores = Arrays.asList(
+            Arrays.asList("Kearny", "Harrison", "WestOrange"),
+            Arrays.asList("Paterson", "Trenton"),
+            Arrays.asList("Chester", "Mansfield", "Haledon"),
+            Arrays.asList("JerseyCity", "Hoboken", "Newport"),
+            Arrays.asList("ExchangePl", "GroveSt", "Newark")
+    );
+
+    private final static int NUMBER_OF_ITEMS = 10;
+    private int count = 0;
+
+    public List<Item> getMultipleItems(int numberOfItems) {
+        Random r = new Random();
+        return IntStream
+                .range(0, numberOfItems)
+                .mapToObj(i ->
+                        Item.builder()
+                                .productID(UUID.randomUUID())
+                                .name(names.get(pickIndex()))
+                                .color(colors.get(pickIndex()))
+                                .price(r.nextInt(100) * r.nextDouble())
+                                .yearFirstAvailable(r.nextInt(101) + 1900)
+                                .nearbyStores(nearbyStores.get(pickIndex()))
+                                .build()
+                )
+                .collect(Collectors.toList());
+    }
+
+    private int pickIndex() {
+        final var inputListSize = names.size();
+        count = count >= inputListSize ? 0 : count++;
+        System.out.println("Count " + count);
+        return count++;
+    }
+}
--- src/main/java/com/bijoyketan/springreactivepractice/domain/Item.java
+++ src/main/java/com/bijoyketan/springreactivepractice/domain/Item.java
@@ -4,18 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
+import java.util.UUID;
 
+@Document
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Item {
+
+    @Id
+    private UUID productID;
     private String name;
-    private double weightInKg;
-    private int yearFirstAvailable;
-    private List<String> nearbyStores;
     private String color;
     private double price;
+    private int yearFirstAvailable;
+    private List<String> nearbyStores;
 }
